d2382399035d18e45b4f40d5173b6e450df83ced
Calvin Yeh <cyeh@mpiwg-berlin.mpg.de>
2022-02-21 13:56:34 +0100

    Enhancement: upgrade @chmap/utilities and @chmap/leaflet-extensions to the latest version

--- example/js/index.js
+++ example/js/index.js
@@ -18,11 +18,11 @@ function initMap() {
     });
 
     map.whenReady(
-    () => {
-        //sometimes, leaflet component doesn't expand its height in a good result initially.
-        //trigger a window resize event to force it to re-calculate its available height again.
-        window.dispatchEvent(new Event('resize'));
-    }
+        () => {
+            //sometimes, leaflet component doesn't expand its height in a good result initially.
+            //trigger a window resize event to force it to re-calculate its available height again.
+            window.dispatchEvent(new Event('resize'));
+        }
     );
 
     DataLayerController.init(map);
